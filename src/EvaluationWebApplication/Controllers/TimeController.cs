a8fba1a07a00f9554cdae8b3b7aa3228df13bfbc
cft-gbielecki <grzegorz.bielecki@cfteurope.pl>
2016-12-06 16:03:10 +0100

    Creating ModelView for timeEntries.

--- src/EvaluationWebApplication/Controllers/TimeController.cs
+++ src/EvaluationWebApplication/Controllers/TimeController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using EvaluationWebApplication.Models.CFT;
+using EvaluationWebApplication.Models.ViewModels;
 
 namespace EvaluationWebApplication.Controllers
 {
@@ -12,15 +13,16 @@ namespace EvaluationWebApplication.Controllers
     public class TimeController : Controller
     {
         private CFTDbContext context = new CFTDbContext();
-        private Employee employee;
+        private TimeEntryViewModel timeModel = new TimeEntryViewModel();
+        //private Employee employee;
 
         public IActionResult Index(string email)
         {
-            employee = context.Employees.FirstOrDefault(empl => empl.Email == email);
-            List<TimeEntry> TimeEntries = new List<TimeEntry>();
-            if (employee.TimeEntries != null)
-                TimeEntries = employee.TimeEntries.Where(timeEntry => timeEntry.Date.Month == DateTime.Today.Month && timeEntry.Date.Year == DateTime.Today.Year).ToList();
-            return View(TimeEntries);
+            timeModel.Employee = context.Employees.FirstOrDefault(empl => empl.Email == email);
+            //List<TimeEntry> TimeEntries = new List<TimeEntry>();
+            if (timeModel.Employee.TimeEntries != null)
+                timeModel.TimeEntries = timeModel.Employee.TimeEntries.Where(timeEntry => timeEntry.Date.Month == DateTime.Today.Month && timeEntry.Date.Year == DateTime.Today.Year).ToList();
+            return View(timeModel);
         }
     }
 }
--- src/EvaluationWebApplication/Models/ViewModels/TimeEntryViewModel.cs
+++ src/EvaluationWebApplication/Models/ViewModels/TimeEntryViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EvaluationWebApplication.Models.CFT;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,22 @@ namespace EvaluationWebApplication.Models.ViewModels
 {
     public class TimeEntryViewModel
     {
+        public Employee Employee { get; set; }
+        public List<TimeEntry> TimeEntries { get; set; }
+        public List<Clients> Clients { get; set; }
+        public List<ServiceClass> Services { get; set; }
+
+        private CFTDbContext context = new CFTDbContext();
+        public TimeEntryViewModel()
+        {
+            foreach(EvaluationWebApplication.Models.CFT.Services service in Enum.GetValues(typeof(EvaluationWebApplication.Models.CFT.Services)))
+            {
+                Services.Add(new ServiceClass(service));
+            }
+            Clients = context.Clients.ToList();
+        }
     }
+
+
+
 }
